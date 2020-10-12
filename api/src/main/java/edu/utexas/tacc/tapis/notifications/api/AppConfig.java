9b4f6da3373373725a499b3985697711a40c897e
Joe Meiring <jmeiring@tacc.utexas.edu>
2020-10-12 15:53:37 -0500

    init v2

---
+++ api/src/main/java/edu/utexas/tacc/tapis/notifications/api/AppConfig.java
@@ -0,0 +1,37 @@
+package edu.utexas.tacc.tapis.notifications.api;
+
+import edu.utexas.tacc.tapis.files.lib.services.NotificationsService;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+import javax.websocket.server.ServerEndpointConfig.Configurator;
+
+
+/**
+ * Instantiates WebSocket end-point with a custom injector so that @Inject can be
+ * used normally.
+ */
+public class AppConfig extends Configurator {
+    private ServiceLocator serviceLocator;
+
+    public AppConfig() {
+        serviceLocator = ServiceLocatorUtilities.createAndPopulateServiceLocator();
+        ServiceLocatorUtilities.bind(serviceLocator, new AbstractBinder() {
+
+            @Override
+            protected void configure() {
+                bind(NotificationsService.class).to(NotificationsService.class);
+                bind(NotificationsResource.class).to(NotificationsResource.class);
+                // Add any other bindings you might need
+
+            }
+        });
+    }
+
+    @Override
+    public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException
+    {
+        return serviceLocator.getService(endpointClass);
+    }
+}
