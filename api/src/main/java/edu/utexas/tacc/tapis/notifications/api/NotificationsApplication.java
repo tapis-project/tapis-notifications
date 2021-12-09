2acf29fa538e4258a173feeceafa8be8eaf237ac
Steve Black <scblack@tacc.utexas.edu>
2021-12-09 13:39:16 -0600

    Switch jooq logging to WARN.

--- api/src/main/java/edu/utexas/tacc/tapis/notifications/api/NotificationsApplication.java
+++ api/src/main/java/edu/utexas/tacc/tapis/notifications/api/NotificationsApplication.java
@@ -70,8 +70,6 @@ public class NotificationsApplication extends ResourceConfig
     // Register classes needed for returning a standard Tapis response for non-Tapis exceptions.
     register(TapisExceptionMapper.class);
     register(ValidationExceptionMapper.class);
-    // Register service class for calling init method during application startup
-//    register(NotificationsServiceImpl.class);
 
     // We specify what packages JAX-RS should recursively scan to find annotations. By setting the value to the
     // top-level directory in all projects, we can use JAX-RS annotations in any tapis class. In particular, the filter
